110919d2a99770df46bf7b2286087d8d36e7ca52
itsayush30 <itsayush30@gmail.com>
2024-02-11 21:47:21 +0530

    Setup Added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Head from './components/Head';
+
+function App() {
+  return (
+    <div>
+    <Head />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Head.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Head = () => {
+  return <div className="text-black-300 bg-blue-400 h-28 text-center">Head
+    
+  </div>;
+};
+
+export default Head;
